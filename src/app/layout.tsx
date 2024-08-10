d04d02ae2c48fe3edda821b2095fe0760d709a4c
Ardi S <ardisa2711@gmail.com>
2024-08-10 12:02:49 +0800

    refactor: update folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
-import "./globals.css";
+
+import type { Metadata } from "next";
+import "@/styles/globals.css";
 
 import { cn } from "@/lib/utils";
 
